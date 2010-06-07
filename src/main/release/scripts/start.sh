c09aa45cafb507db7619263750a98fc00445c005
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2010-06-07 17:04:28 +0000

    - remove pid-file when stopping iPlug

--- src/main/release/scripts/start.sh
+++ src/main/release/scripts/start.sh
@@ -26,22 +26,30 @@ fi
 # functions
 stopIplug()
 {
-  echo "Try stopping jetty ($INGRID_HOME)..."
+  echo "Try stopping ingrid component ($INGRID_HOME)..."
   if [ -f $PID ]; then
-      procid=`cat $PID`
-      idcount=`ps -p $procid | wc -l`
-      if [ $idcount -eq 2 ]; then
-        echo stopping $command
-        kill -9 `cat $PID`
-        echo "process ($procid) has been terminated."
-      else
-        echo "process is not running. Exit."
-        exit 1
-      fi
-    else
-      echo "process is not running. Exit."
-      exit 1
-    fi
+		procid=`cat $PID`
+		idcount=`ps -p $procid | wc -l`
+		if [ $idcount -eq 2 ]; then
+			echo stopping $command
+			kill `cat $PID`
+			sleep 3
+			idcount=`ps -p $procid | wc -l`
+			if [ $idcount -eq 1 ]; then
+				echo "process ($procid) has been terminated."
+				unlink $PID
+				exit 0
+			else
+				echo "process is still running. Exit."
+				exit 1
+			fi 
+		else
+			echo "process is not running. Exit."
+			unlink $PID 
+		fi
+	else
+		echo "process is not running. Exit."
+	fi
 }
 
 stopNoExitIplug()
@@ -95,6 +103,8 @@ startIplug()
     echo "run with heapsize $JAVA_HEAP_MAX"
   fi
 
+	INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME"
+	
   # run it
   exec nohup "$JAVA" $INGRID_HEAPSIZE $INGRID_OPTS -jar jetty/start.jar > console.log &
   
